e97be69f86457015943e7a7638050029a428ff8f
co6tter <34955642+co6tter@users.noreply.github.com>
2025-08-09 15:35:13 +0900

    feat: add role

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -3,7 +3,17 @@ output "bucket_name" {
   value       = module.s3.bucket_name
 }
 
-output "cdn_domain" {
-  description = "Cloudfront domain name"
+output "cf_domain" {
+  description = "CloudFront domain name"
   value       = module.cloudfront.cloudfront_domain
+}
+
+output "cf_distribution_id" {
+  description = "CloudFront Distribution ID"
+  value       = module.cloudfront.cloudfront_distribution_id
+}
+
+output "github_deploy_role_arn" {
+  description = "IAM role ARN assumed by GitHub Actions"
+  value       = aws_iam_role.github_deploy.arn
 }
